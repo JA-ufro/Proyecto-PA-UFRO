f5b5e332e6de65823c613fe35bc89bc1b22fde6b
JA-ufro <142766052+JA-ufro@users.noreply.github.com>
2024-09-25 21:46:55 -0300

    sad
    asd

---
+++ Pryct/src/main/java/org/example/Busqueda.java
@@ -0,0 +1,10 @@
+public class Busqueda {
+
+	private String aBuscar;
+
+	public void filtrar() {
+		// TODO - implement Busqueda.filtrar
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Pryct/src/main/java/org/example/Cancion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Cancion {
+
+	Collection<Usuario> pertenece;
+	private String autor;
+	private String album;
+	private Date fechaLanzamiento;
+	private String nombre;
+
+}
