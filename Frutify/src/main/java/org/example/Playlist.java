2c6fd3e4c7b67a3de2d6c9c6e6fda07fb4f7d33e
JA-ufro <142766052+JA-ufro@users.noreply.github.com>
2024-09-27 12:23:54 -0300

    Add files via upload

---
+++ Frutify/src/main/java/org/example/Playlist.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Playlist {
+
+	Collection<Cancion> canciones;
+	private String nombre;
+
+}
---
+++ Frutify/src/main/java/org/example/Usuario.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Usuario {
+
+	Collection<Playlist> suPlaylist;
+	Collection<Cancion> favoritos;
+	private String nombre;
+	private String contraseña;
+
+	public void iniciarSesion() {
+		// TODO - implement Usuario.iniciarSesion
+		throw new UnsupportedOperationException();
+	}
+
+	public void crearPlaylist() {
+		// TODO - implement Usuario.crearPlaylist
+		throw new UnsupportedOperationException();
+	}
+
+	public void agregarFavoritos() {
+		// TODO - implement Usuario.agregarFavoritos
+		throw new UnsupportedOperationException();
+	}
+
+}
