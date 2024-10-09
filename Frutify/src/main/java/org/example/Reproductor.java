56a9bbe92369e2d76a20be469806601fbe83449b
rayen ancamilla <152819706+Ryenka@users.noreply.github.com>
2024-10-08 22:25:27 -0300

    Add files via upload

--- Frutify/src/main/java/org/example/Reproductor.java
+++ Frutify/src/main/java/org/example/Reproductor.java
@@ -1,24 +1,43 @@
 package org.example;
 
 public class Reproductor {
-
+	private Cancion cancion;
 	public void iniciarCancion() {
 		// TODO - implement Reproductor.iniciarCancion
+		if (cancion != null){
+
+		}
 		throw new UnsupportedOperationException();
 	}
 
 	public void saltarCancion() {
 		// TODO - implement Reproductor.saltarCancion
+		if(cancion != null){
+
+		}
 		throw new UnsupportedOperationException();
 	}
 
 	public void pausarCancion() {
 		// TODO - implement Reproductor.pausarCancion
+		if(cancion != null ){
+
+		}
+		throw new UnsupportedOperationException();
+	}
+
+	public void ReanudadCancion(){
+		if (cancion != null){
+
+		}
 		throw new UnsupportedOperationException();
 	}
 
 	public void cancionAnterior() {
 		// TODO - implement Reproductor.cancionAnterior
+		if (cancion != null){
+
+		}
 		throw new UnsupportedOperationException();
 	}
 
