a1ccbf940bc18799699aae208bd82ad460d78908
E.Cancino <116973712+Nero-test@users.noreply.github.com>
2024-10-11 11:32:51 -0300

    Add files via upload
    modificacion de clase Cancion agregando comentarios y JavaDocs

--- Frutify/src/main/java/org/example/Usuario.java
+++ Frutify/src/main/java/org/example/Usuario.java
@@ -1,59 +1,27 @@
 package org.example;
 
-/**
- * La clase {@code Usuario} representa un usuario con un nombre y una contraseña.
- * Permite almacenar y recuperar el nombre y la contraseña, así como verificar
- * la autenticidad de la contraseña.
- *
- * <p> Esta clase puede ser utilizada para gestionar la autenticación de usuarios en sistemas donde se requiera
- * almacenar y verificar información básica del usuario. </p>
- *
- * @author JA-Ufro
- * @version 1.0
- */
+import java.util.*;
+
 public class Usuario {
 
+	Collection<Playlist> suPlaylist;
+	Collection<Cancion> favoritos;
 	private String nombre;
 	private String contrasena;
 
-	/**
-	 * Constructor que inicializa un nuevo objeto {@code Usuario} con un nombre y una contraseña.
-	 *
-	 * @param nombre El nombre del usuario.
-	 * @param contrasena La contraseña del usuario.
-	 */
-	public Usuario(String nombre, String contrasena) {
-		this.nombre = nombre;
-		this.contrasena = contrasena;
+	public void iniciarSesion() {
+		// TODO - implement Usuario.iniciarSesion
+		throw new UnsupportedOperationException();
 	}
 
-
-	/**
-	 * Devuelve el nombre del usuario.
-	 *
-	 * @return El nombre del usuario.
-	 */
-	public String getNombre() {
-		return nombre;
+	public void crearPlaylist() {
+		// TODO - implement Usuario.crearPlaylist
+		throw new UnsupportedOperationException();
 	}
 
-	/**
-	 * Devuelve la contraseña del usuario.
-	 *
-	 * @return La contraseña del usuario.
-	 */
-	public String getContrasena() {
-		return contrasena;
-	}
-
-	/**
-	 * Verifica si la contraseña proporcionada coincide con la contraseña del usuario.
-	 *
-	 * @param contrasena La contraseña a verificar.
-	 * @return {@code true} si la contraseña es correcta, {@code false} en caso contrario.
-	 */
-	public boolean verificarContrasena(String contrasena) {
-		return this.contrasena.equals(contrasena);
+	public void agregarFavoritos() {
+		// TODO - implement Usuario.agregarFavoritos
+		throw new UnsupportedOperationException();
 	}
 
 }
