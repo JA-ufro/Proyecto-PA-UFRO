49a03cdf4d328426f21f11d35f2450cb039c7713
Pancho36 <151878674+Pancho36@users.noreply.github.com>
2024-10-11 10:52:42 -0300

    Agregacion de los comentarios Previo a la creacion de java doc
    se agregan comentarios

--- Frutify/src/main/java/org/example/Playlist.java
+++ Frutify/src/main/java/org/example/Playlist.java
@@ -2,19 +2,46 @@ package org.example;
 
 import java.util.*;
 import java.util.stream.Collectors;
+/**
+ * La clase Playlist representa una lista de canciones que puede ser manipulada mediante diversas operaciones.
+ * Permite agregar, ordenar y seleccionar canciones de manera aleatoria.
+ *
+ * @autor Francisco Ceballos
+ */
+
 
 public class Playlist {
-
+	// Lista que contiene las canciones de la playlist.
 	List<Cancion> cancions= new ArrayList<>();
+
+	// Nombre de la playlist.
 	private String nombre;
 
+	/**
+	 * Obtiene el nombre de la playlist.
+	 *
+	 * @return el nombre de la playlist.
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * Establece el nombre de la playlist.
+	 *
+	 * @param nombre el nombre de la playlist.
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
+	/**
+	 * Este método se encarga de ordenar la playlist de manera alfabética
+	 * según el nombre de las canciones utilizando la clase Collections.
+	 * El orden es insensible a mayúsculas y minúsculas.
+	 *
+	 * @autor Francisco Ceballos
+	 */
 
 	public void ordenarAlfabeticamente(){
 		Collections.sort(cancions, new Comparator<Cancion>(){
@@ -24,6 +51,13 @@ public class Playlist {
 			}
 		});
 	}
+
+	/**
+	 * Este método selecciona una canción aleatoria de la playlist.
+	 * Si la lista de canciones está vacía, retorna null.
+	 *
+	 * @return una canción aleatoria de la playlist, o null si la lista está vacía.
+	 */
 	public Cancion obtenerCancionAleatoria() {
 		if (cancions.isEmpty()) {
 			return null; // Si la lista está vacía, retorna null
