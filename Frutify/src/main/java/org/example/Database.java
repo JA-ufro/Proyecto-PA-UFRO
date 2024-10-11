35ffc767c12cad47231299d26a527e1920b2e9ea
JA-ufro <142766052+JA-ufro@users.noreply.github.com>
2024-10-11 11:49:51 -0300

    Actualizacion comentarios Javadoc

--- Frutify/src/main/java/org/example/Database.java
+++ Frutify/src/main/java/org/example/Database.java
@@ -23,6 +23,8 @@ public class Database {
     /**
      * Establece una conexión con la base de datos utilizando los parámetros de conexión configurados.
      * Si la conexión se realiza con éxito, muestra un mensaje en la consola; de lo contrario, muestra un error.
+     *
+     * @throws SQLException si ocurre un error al intentar conectar con la base de datos.
      */
     public void conectar(){
         try{
@@ -36,6 +38,8 @@ public class Database {
     /**
      * Cierra la conexión con la base de datos si está abierta. Si la desconexión es exitosa,
      * muestra un mensaje en la consola; de lo contrario, muestra un error.
+     *
+     * @throws SQLException si ocurre un error al intentar cerrar la conexión.
      */
     public void desconectar(){
         if(connection != null){
@@ -54,6 +58,7 @@ public class Database {
      * @param nombre El nombre del usuario.
      * @param contrasena La contraseña del usuario.
      * @return Un objeto {@code Usuario} si las credenciales son correctas; {@code null} en caso contrario.
+     * @throws SQLException si ocurre un error al intentar ejecutar la consulta de autenticación.
      */
     public Usuario iniciarSesion(String nombre, String contrasena){
         String sql = "Select nombre, contrasena from usuario where nombre = ? and contrasena = ?";
@@ -79,6 +84,7 @@ public class Database {
      *
      * @param nombre El nombre de la playlist.
      * @param usuario El objeto {@code Usuario} que está creando la playlist.
+     * @throws SQLException si ocurre un error al intentar insertar la nueva playlist en la base de datos.
      */
     public void crearPlaylist(String nombre, Usuario usuario){
         String sql = "Insert into playlist (nombre, usuario) values (?,?)";
@@ -102,6 +108,7 @@ public class Database {
      *
      * @param nombre El nombre del usuario.
      * @return El ID del usuario si se encuentra; {@code -1} si no se encuentra o si ocurre un error.
+     * @throws SQLException si ocurre un error al intentar recuperar el ID del usuario.
      */
     private int getUsuarioID(String nombre){
         String sql = "Select id from usuario where nombre = ?";
